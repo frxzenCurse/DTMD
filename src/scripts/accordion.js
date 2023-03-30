5b6bd9009f260b09da5ac340d6626dcc07f022e8
frxzenCurse <nikitaableev@mail.ru>
2023-03-30 10:17:27 +0300

    #Build

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,16 +1,29 @@
-function accordionDropdown(e, root) {
-  const accordion = $(root)
-  const target = $(e.target)
+// function accordionDropdown(e, root) {
+//   const accordion = $(root)
+//   const target = $(e.target)
 
-  if (target.closest('[data-accordion-button]').length) {
-    alert('CLICKED')
-    target.closest(accordion).toggleClass('active');
-    target.closest(accordion).find('[data-accordion-dropdown]').eq(0).slideToggle()
-  }
-}
+//   if (target.closest('[data-accordion-button]').length) {
+//     target.closest(accordion).toggleClass('active');
+//     target.closest(accordion).find('[data-accordion-dropdown]').eq(0).slideToggle()
+//   }
+// }
 
-$(() => {
-  window.addEventListener('click', (e) => {
-    accordionDropdown(e, '[data-accordion]')
+// $(() => {
+//   window.addEventListener('click', (e) => {
+//     accordionDropdown(e, '[data-accordion]')
+//   })
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('[data-accordion]')
+
+  accordions.forEach(item => {
+    const drop = item.querySelector('[data-accordion-dropdown]')
+    const button = item.querySelector('[data-accordion-button]')
+
+    button.onclick = function() {
+      $(drop).slideToggle()
+      item.classList.toggle('active')
+    }
   })
 })
