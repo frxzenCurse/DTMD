4b75b50be835d03bbbe421bf6648805a9fc042a5
Nikita <nikita.ableev@mail.ru>
2023-04-18 21:24:22 +0300

    fixes

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -20,10 +20,20 @@ document.addEventListener('DOMContentLoaded', () => {
   accordions.forEach(item => {
     const drop = item.querySelector('[data-accordion-dropdown]')
     const button = item.querySelector('[data-accordion-button]')
+    const isCatalog = item.hasAttribute('data-catalog-accordion')
 
     button.onclick = function() {
+      if (isCatalog) {
+        const activeAccordion = document.querySelector('[data-catalog-accordion].active')
+
+        if (activeAccordion && item !== activeAccordion) {
+          $(activeAccordion.querySelector('[data-accordion-dropdown]')).slideUp()
+          activeAccordion.classList.remove('active')
+        }
+      }
       $(drop).slideToggle()
       item.classList.toggle('active')
+
     }
   })
 })
