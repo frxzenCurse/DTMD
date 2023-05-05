90f766835d1dac4e7af90edcb8226db28e935cce
cloudmill <info@cloudmill.ru>
2023-05-05 15:45:43 +0300

    update

--- src/app.js
+++ src/app.js
@@ -14,7 +14,9 @@ import mask from "inputmask";
 
 // phone mask
 {
-  Inputmask({ mask: "+7 (999) 999-99-99", showMaskOnHover: false }).mask(
-    "[data-mask-phone]"
-  )
+  document.addEventListener('DOMContentLoaded', () => {
+    Inputmask({ mask: "+7 (999) 999-99-99", showMaskOnHover: false }).mask(
+      "[data-mask-phone]"
+    )
+  })
 }
